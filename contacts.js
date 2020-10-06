4de5d6d16476b16c623260ecb469b9feab15a503
Kostya Polishko <kost927@gmail.com>
2020-10-06 10:53:01 +0300

    done

--- contacts.js
+++ contacts.js
@@ -1,21 +1,44 @@
+const { v4: uuidv4 } = require("uuid");
 const fs = require("fs");
 const path = require("path");
 
+const { promises: fsPromise } = fs;
+
 const contactsPath = path.join(__dirname, "./db/contacts.json");
 
-
-function listContacts() {
-    // ...твой код
+async function listContacts() {
+  // ...твой код
+  return JSON.parse(await fsPromise.readFile(contactsPath, "utf-8"));
 }
 
-function getContactById(contactId) {
-    // ...твой код
+async function getContactById(contactId) {
+  // ...твой код
+  const result = await listContacts();
+  const idUser = result.find(user => user.id === contactId);
+
+  return idUser;
 }
 
-function removeContact(contactId) {
-    // ...твой код
+async function removeContact(contactId) {
+  // ...твой код
+  const data = await listContacts();
+  const idUser = data.filter(user => user.id !== contactId);
+
+  return idUser;
 }
 
-function addContact(name, email, phone) {
-    // ...твой код
+async function addContact(name, email, phone) {
+  // ...твой код
+  const data = await listContacts();
+  const id = uuidv4();
+  const addedUser = { id, name, email, phone };
+
+  const updatedList = [...data, addedUser];
+
+  await fsPromise.writeFile(contactsPath, JSON.stringify(updatedList), err => {
+    if (err) throw err;
+  });
+  return updatedList;
 }
+
+module.exports = { listContacts, getContactById, removeContact, addContact };
