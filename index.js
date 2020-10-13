9e6798112e04ab400a352b2943812a8cfea659fe
Kostya Polishko <kost927@gmail.com>
2020-10-13 23:13:39 +0300

    init

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const Joi = require("joi");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const { listContacts, getContactById, removeContact, addContact } = require("./contacts");
@@ -7,47 +8,54 @@ const { listContacts, getContactById, removeContact, addContact } = require("./c
 const app = express();
 const PORT = 3000;
 
-dotenv.config()
+dotenv.config();
 
-app.use(express.json())
+app.use(express.json());
 
-app.get('/contacts', async (req, res, next) => {
-    const contacts = await listContacts();
-    res.json(contacts)
-
-})
-
-app.get('/contacts/:contactId', async (req, res) => {
-    const { contactId } = req.params;
-    const contact = await getContactById(+contactId);
-    if (contact) {
-        res.status(200).json(contact);
-        return;
-    }
-    res.status(404).json('Not found');
-   
-})
-
-app.post('/contacts', async (req, res) => {
-    const { name, email, phone } = req.body;
-    if (typeof name === 'string' && name.length && typeof email === 'string' && email.length && phone.length) {
-        const contacts = await addContact(name, email, phone);
-        res.status(201).json(contacts);
-        return;
-    }
-    res.status(400).json('missing required name field');
+app.get("/contacts", async (req, res, next) => {
+  const contacts = await listContacts();
+  res.json(contacts);
 });
 
-// app.delete('/:contactId', async (req, res) => {
-//     const { contactId } = req.params;
-//     const id = await getContactById(+contactId);
-//     if (id) {
-//         await removeContact(+contactId);
-//         res.status(200).json("contact deleted");
-//         return;
-//     }
-//     res.status(404).json("Not found");
-// })
+app.get("/contacts/:contactId", async (req, res) => {
+  const { contactId } = req.params;
+    const contact = await getContactById(+contactId);
+  if (contact) {
+    res.status(200).json(contact);
+    return;
+  }
+  res.status(404).json("Not found");
+});
+
+app.post("/contacts", async (req, res) => {
+    const { name, email, phone } = req.body;
+    const contactSchema = Joi.object({
+        name: Joi.string().required(),
+        email: Joi.string().email().required(),
+        phone: Joi.string().required()
+    });
+
+    const { error } = contactSchema.validate(req.body);
+    if (error) {
+        res.status(400).json('missing required name field');
+        return;
+    } else {
+        const contacts = await addContact(name, email, phone);
+        res.status(201).json(contacts);
+    } 
+});
+
+app.delete('contacts/:contactId', async (req, res) => {
+    const { contactId } = req.params;
+    const id = await getContactById(+contactId);
+    console.log('object', contactId)
+    if (id) {
+        await removeContact(contactId);
+        res.status(200).json("contact deleted");
+        return;
+    }
+    res.status(404).json("Not found");
+})
 
 // app.patch('/:contactId', async (req, res) => {
 //     const { contactId } = req.params;
@@ -61,5 +69,4 @@ app.post('/contacts', async (req, res) => {
 //     res.status(404).json('Not found')
 // })
 
-
-app.listen(PORT, () => console.log(`server started on port ${PORT}`))
+app.listen(PORT, () => console.log(`server started on port ${PORT}`));
