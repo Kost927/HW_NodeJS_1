1e7e1255de57df8557a2bf372b0f8ab9427b3215
Kostya Polishko <kost927@gmail.com>
2020-10-13 19:27:55 +0300

    init2

--- index.js
+++ index.js
@@ -3,37 +3,27 @@ const morgan = require("morgan");
 const cors = require("cors");
 const Joi = require("joi");
 const dotenv = require("dotenv");
-const Users = require("./contacts");
+const { listContacts, getContactById, removeContact, addContact } = require("./contacts");
 
 const app = express();
 const PORT = 3000;
 
 dotenv.config()
 
-app.use((req, res, next) => {
-    console.log('first middleware')
-    console.log(req.query)
-    req.query.name = "Colin"
-    next();
+app.get('/contacts', async (req, res) => {
+    const contacts = await listContacts();
+    res.json(contacts)
+
 })
 
-app.use((req, res, next) => {
-    console.log('second middleware')
-    console.log(req.query)
-    req.query.surname = "Pupkin"
-    next();
-})
-
-app.use((req, res, next) => {
-    console.log('third middleware')
-    console.log(req.query)
-    next();
-})
-
-app.get('/users', async (req, res) => {
-    const users = await Users.getUsers();
-    res.json(users)
-
+contactsRouter.get('/:contactId', async (req, res) => {
+    const { contactId } = req.params;
+    const contact = await getContactById(+contactId);
+    if (contact) {
+        res.json(contact);
+        return;
+    }
+    res.status(404).json('Not found');
 })
 
 app.listen(PORT, () => console.log(`server started on port ${PORT}`))
